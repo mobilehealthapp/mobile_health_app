b990443c198a5c2ba3207a725b9a3c19d84d078d
bhattirahat <bhattir@hotmail.co.uk>
2021-06-23 23:30:25 -0400

    Added drawers

---
+++ lib/Profile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_health_app/drawers.dart';
+
+// ignore: camel_case_types
+class Profile_Page extends StatelessWidget {
+  const Profile_Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawers(),
+      appBar: AppBar(
+        title: Text('Profile Page'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_health_app/drawers.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,63 +12,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mobile Health App',
       theme: ThemeData(
-        primaryColor: Color(0xFF00BCD4),
-        primaryColorDark: Color(0xFF0097A7),
-        primaryColorLight: Color(0xFFB2EBF2),
-        accentColor: Color(0xFF607D8B),
-        textTheme: TextTheme().apply(
+          primaryColor: Color(0xFF00BCD4),
+          primaryColorDark: Color(0xFF0097A7),
+          primaryColorLight: Color(0xFFB2EBF2),
+          accentColor: Color(0xFF607D8B),
+          textTheme: TextTheme().apply(
             bodyColor: Color(0xFF212121),
             displayColor: Color(0xFF757575),
-        )
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+          )),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title, style: TextStyle(color: Colors.white),),
+        title: Text('HomePage is here'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      drawer: Drawers(),
     );
   }
 }
