fdc3898a21a63236d471642c464a077f35cc20a3
Robert <rwlush@mun.ca>
2021-06-29 15:06:25 -0230

    -changed page layout and image size

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -11,118 +10,127 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(leading: BackButton(onPressed: (){},),
-      title: Text('Login', style: TextStyle(color: Colors.white,))),
+      appBar: AppBar(
+          leading: BackButton(
+            onPressed: () {},
+          ),
+          title: Text('Login',
+              style: TextStyle(
+                color: Colors.white,
+              ))),
       backgroundColor: Colors.white,
-      body: Container(
-        constraints: BoxConstraints.expand(),
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 200.0,
-                height: 200.0,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage('images/logo-1-removebg-preview.png'),
-                  fit: BoxFit.fill),
+      body: SingleChildScrollView(
+        child: Container(
+          constraints: BoxConstraints(),
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 25.0,
                 ),
-              ),
-              SizedBox(height: 20,),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: TextField(
-                  style: TextStyle(
-                    color: Colors.black,
+                Container(
+                  width: 200.0,
+                  height: 200.0,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('images/logo-1-removebg-preview.png'),
+                        fit: BoxFit.fill),
                   ),
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    icon: Icon(
-                      FontAwesomeIcons.user,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: TextField(
+                    style: TextStyle(
                       color: Colors.black,
                     ),
-                    hintText: 'Enter Username',
-                    hintStyle: TextStyle(
-                      fontSize: 20
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10.0),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      icon: Icon(
+                        FontAwesomeIcons.user,
+                        color: Colors.black,
+                      ),
+                      hintText: 'Enter Username',
+                      hintStyle: TextStyle(fontSize: 20),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10.0),
+                        ),
                       ),
                     ),
                   ),
-                )
-                ,
-              ),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: TextField(
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    icon: Icon(
-                      FontAwesomeIcons.key,
+                ),
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: TextField(
+                    style: TextStyle(
                       color: Colors.black,
                     ),
-                    hintText: 'Enter Password',
-                    hintStyle: TextStyle(
-                      fontSize: 20
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10.0),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      icon: Icon(
+                        FontAwesomeIcons.key,
+                        color: Colors.black,
+                      ),
+                      hintText: 'Enter Password',
+                      hintStyle: TextStyle(fontSize: 20),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10.0),
+                        ),
                       ),
                     ),
                   ),
-                )
-                ,
-              ),
-              SizedBox(height: 30,),
-              MaterialButton(
-                minWidth: 250,
-                color: Colors.blueGrey,
-                height: 50.0,
-                onPressed: (){},
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
                 ),
-                child: Text('Patient Login',
-                style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.white,
-                ),),
-              ),
-
-              SizedBox(height: 30,),
-              MaterialButton(
-                minWidth: 250,
-                color: Colors.blueGrey,
-                height: 50.0,
-                onPressed: (){},
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
+                SizedBox(
+                  height: 30,
                 ),
-                child: Text('Physician Login',
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.white,
-                  ),),
-              ),
-
-
-
-
-            ],
+                MaterialButton(
+                  minWidth: 250,
+                  color: Colors.blueGrey,
+                  height: 50.0,
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Text(
+                    'Patient Login',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                MaterialButton(
+                  minWidth: 250,
+                  color: Colors.blueGrey,
+                  height: 50.0,
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Text(
+                    'Physician Login',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
-
-                );
-
+    );
   }
 }
